c2b53ff896fe93a6e2d6f1ee89f6befe95ab850d
jakepagnotto99 <jakepagnotto99@email.com>
2024-10-24 12:44:06 -0400

    working

--- milestone-tracker-app/server/server.js
+++ milestone-tracker-app/server/server.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const dotenv = require('dotenv');
 const db = require('./db');
-
-// Load environment variables
-dotenv.config();
-
 // Import routes
 const userRoutes = require('./routes/userRoutes');
 const milestoneRoutes = require('./routes/milestoneRoutes');

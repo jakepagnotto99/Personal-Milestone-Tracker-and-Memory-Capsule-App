70a18364226877da12ddfe1c371a76b89cce9474
jakepagnotto99 <jakepagnotto99@email.com>
2024-10-24 12:25:50 -0400

    hello

--- milestone-tracker-app/server/server.js
+++ milestone-tracker-app/server/server.js
@@ -27,6 +27,7 @@ app.use('/api/memoryEntries', memoryEntryRoutes);
 app.use('/api/comments', commentRoutes);
 app.use('/api/reactions', reactionRoutes);
 
+
 // Start server
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
